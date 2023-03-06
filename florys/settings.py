8f95e4721c72cfbdbedb61b36f5c53f7a60eeed7
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-03-06 18:28:42 +0000

    Imported context processor  and static files in settings

--- florys/settings.py
+++ florys/settings.py
@@ -16,6 +16,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib import staticfiles
+from django.template.context_processors import media
 
 if os.path.isfile("env.py"):
     import env
@@ -145,12 +147,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
