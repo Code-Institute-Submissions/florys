4490803882f550ec76ca7d31b7e7014939ef8658
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-03-29 18:16:30 +0200

    Added new url to urlpatterns

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('blog/', views.PostList.as_view(), name='blog_home')
+    path('blog/', views.PostList.as_view(), name='blog_home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
