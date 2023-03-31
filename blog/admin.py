b9a2f8d7b734de16dbd39ca61bf566692ae39897
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-03-31 16:52:49 +0200

    Changed admin.py file to display new fields in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -5,11 +5,10 @@ from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'status', 'created_on')
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    summernote_fields = ('body')
 
 
 @admin.register(Comment)
