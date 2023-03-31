0c9c03d768f797439ec11be7f84930fc26f73ba9
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-03-31 19:25:01 +0200

    Added widgets to PostForm and categories to the post creation

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,25 @@
-from .models import Comment, Post
+from .models import Comment, Post, Category
 from django import forms
+
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'body')
+        fields = ('title', 'author', 'category', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
 
 
 class EditForm(forms.ModelForm):
