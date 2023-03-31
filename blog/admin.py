30f10f65a544caa171cfe5fd8a076ae8b6333e08
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-03-31 19:23:48 +0200

    Registered new model Category

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post, Comment
+from .models import Post, Comment, Category
+
+admin.site.register(Category)
 
 
 @admin.register(Post)
