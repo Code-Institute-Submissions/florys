dd59e944c6a3705825e5d56ecac96bbc86e42345
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-04-01 17:37:24 +0200

    Implemented text editor summernote

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from .models import Comment, Post, Category
 from django import forms
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 choices = Category.objects.all().values_list('name', 'name')
 
@@ -10,25 +11,37 @@ for item in choices:
 
 
 class PostForm(forms.ModelForm):
+
     class Meta:
         model = Post
         fields = ('title', 'author', 'category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'body': SummernoteWidget()
         }
 
 
 class EditForm(forms.ModelForm):
+
     class Meta:
         model = Post
         fields = ('title', 'body',)
 
+        widgets = {
+            'body': SummernoteWidget()
+
+        }
+
 
 class CommentForm(forms.ModelForm):
+
     class Meta:
         model = Comment
         fields = ('body',)
+
+        widgets = {
+            'body': SummernoteWidget()
+        }
