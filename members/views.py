9d19a503f27ef3a77d28068f998f1f5aa5c047c9
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-04-02 19:28:28 +0200

    Initialized new views in members to edit user profile

--- members/views.py
+++ members/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from django.views import generic
+from django.contrib.auth.views import PasswordChangeView
+from django.views.generic import DetailView
+from .forms import SignUpForm, EditProfileForm, ChangePasswordForm
+from blog.models import Profile
 
 
 # Create your views here.
@@ -10,3 +12,34 @@ class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('blog_home')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = ChangePasswordForm
+    success_url = reverse_lazy('password_success')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
+
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        users = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context["page_user"] = page_user
+        return context
