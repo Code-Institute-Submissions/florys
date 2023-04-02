7f6f9a75ac78f838a33609e73eb8eb6320d94978
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-04-02 19:23:19 +0200

    Initialized new Profile model for user profile in blog app

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,29 @@ from django.urls import reverse
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = CloudinaryField('image', blank=True)
+    website_url = models.CharField(max_length=200, null=True, blank=True)
+    facebook_url = models.CharField(max_length=200, null=True, blank=True)
+    twitter_url = models.CharField(max_length=200, null=True, blank=True)
+    instagram_url = models.CharField(max_length=200, null=True, blank=True)
+
+    def get_social_media_urls(self):
+        platforms = {
+            'Website': self.website_url,
+            'Facebook': self.facebook_url,
+            'Twitter': self.twitter_url,
+            'Instagram': self.instagram_url,
+        }
+        return [(p, u) for p, u in platforms.items() if u]
+
+
+    def __str__(self):
+        return str(self.user)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
@@ -21,10 +44,11 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     updated_on = models.DateTimeField(auto_now=True)
     body = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', default='placeholder', blank=True)
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=255, default='trips')
+    snippet = models.CharField(max_length=255)
     status = models.IntegerField(choices=STATUS, default=1)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
