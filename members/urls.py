8f007330f779d34b9f61471574c3a88b1655961e
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-04-02 19:27:51 +0200

    Add new urls to urlpatterns for editing info user

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', views.UserRegisterView.as_view(), name='register'),
-]
+                  path('register/', views.UserRegisterView.as_view(), name='register'),
+                  path('edit_profile/', views.UserEditView.as_view(), name='edit_profile'),
+                  path('password/',
+                       views.PasswordsChangeView.as_view(template_name='registration/change_password.html')),
+                  path('password_success', views.password_success, name='password_success'),
+                  path('<int:pk>/profile/', views.ShowProfilePageView.as_view(), name='show_profile_page'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
