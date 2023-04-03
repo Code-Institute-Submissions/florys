26be54ee2a0af41c7656bd1e24381af48b864646
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-04-03 17:33:48 +0200

    Added new fields to CommentForm in forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -11,7 +11,6 @@ for item in choices:
 
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
         fields = ('title', 'author', 'category', 'body', 'snippet', 'featured_image')
@@ -27,7 +26,6 @@ class PostForm(forms.ModelForm):
 
 
 class EditForm(forms.ModelForm):
-
     class Meta:
         model = Post
         fields = ('title', 'body', 'snippet')
@@ -42,8 +40,8 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ('body',)
-
+        fields = ('name', 'body')
         widgets = {
-            'body': SummernoteWidget()
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
