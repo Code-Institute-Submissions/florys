a379d05928d81ed1c19604a050b73478a699f36f
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-04-03 17:34:53 +0200

    Created a profile page view in members and edit profile page

--- members/views.py
+++ members/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.views import PasswordChangeView
-from django.views.generic import DetailView
-from .forms import SignUpForm, EditProfileForm, ChangePasswordForm
+from django.views.generic import DetailView, CreateView
+from .forms import SignUpForm, EditProfileForm, ChangePasswordForm, ProfilePageForm
 from blog.models import Profile
 
 
@@ -32,14 +32,33 @@ def password_success(request):
     return render(request, 'registration/password_success.html', {})
 
 
+class CreateProfilePageView(CreateView):
+
+    model = Profile
+    form_class = ProfilePageForm
+    template_name = 'registration/create_user_profile_page.html'
+
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
 class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
 
     def get_context_data(self, *args, **kwargs):
-        users = Profile.objects.all()
         context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
 
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
         context["page_user"] = page_user
         return context
+
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url']
+
+    success_url = reverse_lazy('blog_home')
