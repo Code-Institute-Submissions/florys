41599d6daa897035581bbdb28b9b0f1002ce1228
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-04-07 19:02:58 +0200

    Added come to summernote field body

--- blog/admin.py
+++ blog/admin.py
@@ -11,7 +11,7 @@ class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
-    summernote_fields = ('body')
+    summernote_fields = ('body',)
 
 
 @admin.register(Comment)
