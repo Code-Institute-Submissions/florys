002e16a2dfe7c97e561f09aa173bd6d49b8cc532
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-04-10 12:25:56 +0200

    Fixed path in url patterns

--- members/urls.py
+++ members/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('logout/', views.AccountLogout.as_view(), name='account_logout'),
     path('edit_profile/', views.UserEditView.as_view(), name='edit_profile'),
     path('password_success', views.password_success, name='password_success'),
-    path('password', views.PasswordsChangeView.as_view(template_name='registration/change_password.html'),
+    path('password/', views.PasswordsChangeView.as_view(template_name='registration/change_password.html'),
          name='change_password'),
     path('<int:pk>/profile/', views.ShowProfilePageView.as_view(), name='show_profile_page'),
     path('<int:pk>/edit_profile_page/', views.EditProfilePageView.as_view(), name='edit_profile_page'),
