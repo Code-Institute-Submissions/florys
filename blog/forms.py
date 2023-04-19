61416d906ae1219da02e5cc561032d29cd9e0d31
Sebastiano Ballotta <76972368+sebhd1@users.noreply.github.com>
2023-04-19 14:15:48 +0500

    Add files via upload

--- blog/forms.py
+++ blog/forms.py
@@ -1,45 +1,45 @@
-from .models import Comment, Post, Category
-from django import forms
-from django_summernote.widgets import SummernoteWidget
+# from .models import Comment, Post, Category
+# from django import forms
+# from django_summernote.widgets import SummernoteWidget
 
-choices = Category.objects.all().values_list('name', 'name')
+# # choices = Category.objects.all().values_list('name', 'name')
 
-choice_list = []
+# choice_list = []
 
-for item in choices:
-    choice_list.append(item)
-
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ('title', 'author', 'category', 'body', 'snippet', 'featured_image')
-
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
-            'body': SummernoteWidget(),
-            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
-        }
+# for item in choices:
+#     choice_list.append(item)
 
 
-class EditForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ('title', 'featured_image', 'body', 'snippet')
+# class PostForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         fields = ('title', 'author', 'category', 'body', 'snippet', 'featured_image')
 
-        widgets = {
-            'body': SummernoteWidget(),
-            'feature_image': forms.ImageField(),
-            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
-        }
+#         widgets = {
+#             'title': forms.TextInput(attrs={'class': 'form-control'}),
+#             'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
+#             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+#             'body': SummernoteWidget(),
+#             'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+#         }
 
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ('name', 'body')
-        widgets = {
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
-        }
+# class EditForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         fields = ('title', 'featured_image', 'body', 'snippet')
+
+#         widgets = {
+#             'body': SummernoteWidget(),
+#             'feature_image': forms.ImageField(),
+#             'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+#         }
+
+
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = ('name', 'body')
+#         widgets = {
+#             'body': forms.Textarea(attrs={'class': 'form-control'}),
+#         }
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.views import generic, View
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from .models import Post, Category, Comment
-from .forms import CommentForm, EditForm, PostForm
+# from .forms import CommentForm, EditForm, PostForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.contrib import messages
@@ -27,100 +27,100 @@ class HomeBlogView(ListView):
         return context
 
 
-def CategoryListView(request):
-    cat_menu_list = Category.objects.all()
-    return render(request, 'post/category_list.html', {'cat_menu_list': cat_menu_list})
+# def CategoryListView(request):
+#     cat_menu_list = Category.objects.all()
+#     return render(request, 'post/category_list.html', {'cat_menu_list': cat_menu_list})
 
 
-def CategoryView(request, cats):
-    category_posts = Post.objects.filter(category=cats)
-    return render(request, 'post/categories.html', {
-        'cats': cats.title().replace('-', ' '),
-        'category_posts': category_posts
-    })
+# def CategoryView(request, cats):
+#     category_posts = Post.objects.filter(category=cats)
+#     return render(request, 'post/categories.html', {
+#         'cats': cats.title().replace('-', ' '),
+#         'category_posts': category_posts
+#     })
 
 
-class AddPostView(CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'post/add_post.html'
+# class AddPostView(CreateView):
+#     model = Post
+#     form_class = PostForm
+#     template_name = 'post/add_post.html'
 
-    def form_valid(self, form):
-        messages.success(self.request, 'Post Created in the Blog!')
-        return super().form_valid(form)
-
-
-class AddCategoryView(CreateView):
-    model = Category
-    template_name = 'post/add_category.html'
-    fields = '__all__'
+#     def form_valid(self, form):
+#         messages.success(self.request, 'Post Created in the Blog!')
+#         return super().form_valid(form)
 
 
-class UpdatePostView(UpdateView):
-    model = Post
-    form_class = EditForm
-    template_name = 'post/update_post.html'
-
-    def form_valid(self, form):
-        messages.success(self.request, 'Post updated!')
-        return super().form_valid(form)
+# class AddCategoryView(CreateView):
+#     model = Category
+#     template_name = 'post/add_category.html'
+#     fields = '__all__'
 
 
-class DeletePostView(DeleteView):
-    model = Post
-    template_name = 'post/delete_post.html'
-    success_url = reverse_lazy('blog_home')
+# class UpdatePostView(UpdateView):
+#     model = Post
+#     form_class = EditForm
+#     template_name = 'post/update_post.html'
 
-    def form_valid(self, form):
-        messages.success(self.request, 'Your post was removed!')
-        return super().form_valid(form)
-
-
-class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "post/index.html"
-    paginate_by = 6
+#     def form_valid(self, form):
+#         messages.success(self.request, 'Post updated!')
+#         return super().form_valid(form)
 
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'post/post_detail.html'
+# class DeletePostView(DeleteView):
+#     model = Post
+#     template_name = 'post/delete_post.html'
+#     success_url = reverse_lazy('blog_home')
 
-    def get_context_data(self, *args, **kwargs):
-        cat_menu = Category.objects.all()
-        likes = self.object
-        total_likes = likes.number_of_likes()
-        context = super(PostDetailView, self).get_context_data(*args, **kwargs)
-        context["cat_menu"] = cat_menu
-        context["total_likes"] = total_likes
-        return context
+#     def form_valid(self, form):
+#         messages.success(self.request, 'Your post was removed!')
+#         return super().form_valid(form)
 
 
-class PostLike(View):
-
-    def post(self, request, pk, *args, **kwargs):
-        post = get_object_or_404(Post, pk=pk)
-        if post.likes.filter(id=request.user.id).exists():
-            post.likes.remove(request.user)
-        else:
-            post.likes.add(request.user)
-
-        return HttpResponseRedirect(reverse('post_detail', args=[str(pk)]))
+# class PostList(generic.ListView):
+#     model = Post
+#     queryset = Post.objects.filter(status=1).order_by("-created_on")
+#     template_name = "post/index.html"
+#     paginate_by = 6
 
 
-class AddCommentView(CreateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'post/add_comment.html'
-    success_url = reverse_lazy('blog_home')
+# class PostDetailView(DetailView):
+#     model = Post
+#     template_name = 'post/post_detail.html'
 
-    def form_valid(self, form):
-        form.instance.post_id = self.kwargs['pk']
-        comment = form.save(commit=False)
-        messages.success(self.request, 'Your comment is published!')
+#     def get_context_data(self, *args, **kwargs):
+#         cat_menu = Category.objects.all()
+#         likes = self.object
+#         total_likes = likes.number_of_likes()
+#         context = super(PostDetailView, self).get_context_data(*args, **kwargs)
+#         context["cat_menu"] = cat_menu
+#         context["total_likes"] = total_likes
+#         return context
 
-        comment.user = self.request.user
-        comment.save()
-        return super().form_valid(form)
+
+# class PostLike(View):
+
+#     def post(self, request, pk, *args, **kwargs):
+#         post = get_object_or_404(Post, pk=pk)
+#         if post.likes.filter(id=request.user.id).exists():
+#             post.likes.remove(request.user)
+#         else:
+#             post.likes.add(request.user)
+
+#         return HttpResponseRedirect(reverse('post_detail', args=[str(pk)]))
+
+
+# class AddCommentView(CreateView):
+#     model = Comment
+#     form_class = CommentForm
+#     template_name = 'post/add_comment.html'
+#     success_url = reverse_lazy('blog_home')
+
+#     def form_valid(self, form):
+#         form.instance.post_id = self.kwargs['pk']
+#         comment = form.save(commit=False)
+#         messages.success(self.request, 'Your comment is published!')
+
+#         comment.user = self.request.user
+#         comment.save()
+#         return super().form_valid(form)
 
