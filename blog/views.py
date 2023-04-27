711acf19d3da2535aba3cdd50db009fe29ff8c97
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-04-27 10:48:21 +0200

    Adjusted pagination

--- blog/views.py
+++ blog/views.py
@@ -80,7 +80,7 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "post/index.html"
-    paginate_by = 6
+    paginate_by = 4
 
 
 class PostDetailView(DetailView):
